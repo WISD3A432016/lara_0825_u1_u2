db71f340d0eea4e853be058187ffbfc78cf2fe65
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-19 18:17:15 +0800

    Exercise 7-2 : change()

--- database/migrations/2017_10_19_092747_create_posts_table.php
+++ database/migrations/2017_10_19_092747_create_posts_table.php
@@ -20,6 +20,8 @@ class CreatePostsTable extends Migration
             //修改 migration 檔案的內容
             $table->text('post');
             $table->timestamps();
+            $table -string('title',500) ->change();
+
         });
     }
 
