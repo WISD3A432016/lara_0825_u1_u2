de0b295930cce642d855198ff7c28abc6daa478e
Jing-Ru Sun <3a432016@gm.student.ncut.edu.tw>
2017-11-03 14:51:08 +0800

    執行http://localhost:8000/test，多新增幾筆posts資料表內的資料

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,14 @@ Route::get('/test', function () {
         'content'=>'test content',
     ]);
    */
-   /*
+
     $post = new \App\Post();
     $post -> title = 'test title';
     $post -> content ='test content';
     $post -> save();
-   */
+   
+    /*
     $posts = \App\Post::all();
     dd($posts);
-
+    */
 });
