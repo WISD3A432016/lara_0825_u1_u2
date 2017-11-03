cef28e7985e754eb6f71a220f00bf7a507f5de5c
Jing-Ru Sun <3a432016@gm.student.ncut.edu.tw>
2017-11-03 14:32:03 +0800

    單元一、Eloquent ORM Exercise2-3 :增加Route後，先執行 php artisan migrate 到DB，之後於http://localhost:8000/test做測試

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-\App\Post::create([
-    'title'=>'test title',
-    'content'=>'test content',
-]);
+Route::get('/test', function () {
+    \App\Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+    ]);
+});
