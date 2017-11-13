3f84b12addd7420c8beb97767a884cad84805838
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-13 19:27:49 +0800

    更正錯誤: 單元一、Eloquent ORM Exercise7-3 開啟 Comment.php，增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Post;
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+    protected $fillable=['title', 'content_2', 'is_feature'];
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
