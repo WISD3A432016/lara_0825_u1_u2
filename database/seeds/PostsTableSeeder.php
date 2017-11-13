400456e24570cf3ce0931df8402b37e03334ea97
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-13 18:29:01 +0800

    單元二、Seeding Exercise4-2 : 讓資料有時間序列性、隨機感，並使⽤Carbon 控制⽇期時間產⽣間距，讓測試資料不會都被設定在同⼀天

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Carbon\Carbon;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -16,9 +17,13 @@ class PostsTableSeeder extends Seeder
 
         //練習2
         foreach(range(1,20) as $number){
+            $total=20;
             Post::create([
                 'title' => 'title '.$number,
                 'content' => 'content '.$number,
+                'is_feature' => rand(0,1),
+                'created_at' => Carbon::now() ->subDays($total - $number),
+                'updated_at' => Carbon::now() ->subDays($total - $number),
             ]);
         }
     }
