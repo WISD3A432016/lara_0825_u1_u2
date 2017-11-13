22b33ccc7959cd2837ff3dd8a406be965e5d985e
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-13 17:58:22 +0800

    單元二、Seeding Exercise2-2 :將 PostsTableSeeder 設定至 DatabaseSeeder內(原先資料已存在，因此在上面做註解標示)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CommentsTableSeeder::class);
+        //練習2-2: $this->call(PostsTableSeeder::class);
         $this->call(PostsTableSeeder::class);
     }
 }
